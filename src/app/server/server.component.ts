713493149124090750c0190f30baf72f4a69a9a9
xiaoyingtao22 <lvqian22@gmail.com>
2019-03-28 12:50:04 -0400

    cooking game

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,37 +8,83 @@ import { Student } from '../../shared/student.model';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-    serverId = 10;
-    serverStatus = 'offLine';
-    counter = 0;
-    isLogin = false;
-    userName: string;
-    students: Student[] = [new Student('Bill Gates', 'Computer Science'),
-                           new Student('Steve Jobs', 'Computer Science'),
-                           new Student('Elon Musk', 'Computer Science')];
-
-    getServerStatus() {
-      return this.serverStatus;
+  ingredientcomp: string;
+  ingredients = [];
+  success = false;
+  fail = false;
+  dish: string;
+  dish1 = {tomato: 1, egg: 1};
+  dish2 = {carrot: 1, cucumber: 1, chicken: 1};
+  dish3 = {greenpepper: 1, potato: 1, eggplant: 1};
+  i = 0;
+  counter1 = 0;
+  counter2 = 0;
+  counter3 = 0;
+  serverId = 10;
+  serverStatus = 'offLine';
+  counter = 0;
+  isLogin = false;
+  userName: string;
+  students: Student[] = [new Student('Bill Gates', 'Computer Science'),
+    new Student('Steve Jobs', 'Computer Science'),
+    new Student('Elon Musk', 'Computer Science')];
+  addGredient() {
+    this.success = false;
+    this.fail = false;
+    this.ingredients.push(this.ingredientcomp);
+    this.ingredientcomp = null;
+  }
+  checkGredient() {
+    for (this.i = 0; this.i < this.ingredients.length; this.i++) {
+      if (this.ingredients[this.i] in this.dish1) {
+        this.counter1++;
+      }
+      if (this.ingredients[this.i] in this.dish2) {
+        this.counter2++;
+      }
+      if (this.ingredients[this.i] in this.dish3) {
+        this.counter3++;
+      }
     }
-
-    counterPlus() {
-     this.counter ++;
+    if (this.counter1 === 2) {
+      this.success = true;
+      this.dish = '番茄炒蛋';
+    } else if (this.counter2 === 3) {
+      this.success = true;
+      this.dish = '宫保鸡丁';
+    } else if (this.counter3 === 3) {
+      this.success = true;
+      this.dish = '地三鲜';
+    } else {
+      this.fail = true;
     }
+    this.ingredients.length = 0;
+    this.counter1 = 0;
+    this.counter2 = 0;
+    this.counter3 = 0;
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
-    resetCounter() {
-      this.counter = 0;
-    }
+  counterPlus() {
+    this.counter ++;
+  }
 
-    login() {
-      this.isLogin = true;
-    }
+  resetCounter() {
+    this.counter = 0;
+  }
 
-    signOut() {
-      this.isLogin = false;
-    }
+  login() {
+    this.isLogin = true;
+  }
 
-    // Event Binding
-    onUpdateUserName(event: Event) {
-      this.userName = (<HTMLInputElement>event.target).value;
-    }
+  signOut() {
+    this.isLogin = false;
+  }
+
+  // Event Binding
+  onUpdateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
 }
